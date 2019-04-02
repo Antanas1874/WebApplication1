d4eb354fd8a2dcf76db2405ef1255728d8f3b185
Domantas123 <domce99@gmail.com>
2019-04-02 08:17:37 +0300

    Login and Register

--- WebApplication1/ClientApp/src/App.js
+++ WebApplication1/ClientApp/src/App.js
@@ -4,6 +4,8 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+import { Login } from "./components/Login";
+import { Register } from "./components/Register";
 
 export default class App extends Component {
   displayName = App.name
@@ -13,7 +15,9 @@ export default class App extends Component {
       <Layout>
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
-        <Route path='/fetchdata' component={FetchData} />
+            <Route path='/fetchdata' component={FetchData} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
       </Layout>
     );
   }
--- WebApplication1/ClientApp/src/components/NavMenu.js
+++ WebApplication1/ClientApp/src/components/NavMenu.js
@@ -5,37 +5,48 @@ import { LinkContainer } from 'react-router-bootstrap';
 import './NavMenu.css';
 
 export class NavMenu extends Component {
-  displayName = NavMenu.name
+    displayName = NavMenu.name
 
-  render() {
-    return (
-      <Navbar inverse fixedTop fluid collapseOnSelect>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <Link to={'/'}>WebApplication1</Link>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav>
-            <LinkContainer to={'/'} exact>
-              <NavItem>
-                <Glyphicon glyph='home' /> Home
+    render() {
+        return (
+            <Navbar inverse fixedTop fluid collapseOnSelect>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <Link to={'/'}>eShop.lt</Link>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav>
+                        <LinkContainer to={'/'} exact>
+                            <NavItem>
+                                <Glyphicon glyph='home' /> Apie mus
               </NavItem>
-            </LinkContainer>
-            <LinkContainer to={'/counter'}>
-              <NavItem>
-                <Glyphicon glyph='education' /> Counter
+                        </LinkContainer>
+                        <LinkContainer to={'/counter'}>
+                            <NavItem>
+                                <Glyphicon glyph='education' /> Counter
               </NavItem>
-            </LinkContainer>
-            <LinkContainer to={'/fetchdata'}>
-              <NavItem>
-                <Glyphicon glyph='th-list' /> Fetch data
+                        </LinkContainer>
+                        <LinkContainer to={'/fetchdata'}>
+                            <NavItem>
+                                <Glyphicon glyph='th-list' /> Fetch data
               </NavItem>
-            </LinkContainer>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    );
-  }
+                        </LinkContainer>
+                        <LinkContainer to={'/login'}>
+                            <NavItem>
+                                <Glyphicon glyph='log' /> Login
+              </NavItem>
+                        </LinkContainer>
+                        <LinkContainer to={'/register'}>
+                            <NavItem>
+                                <Glyphicon glyph='reg' /> Register
+              </NavItem>
+                        </LinkContainer>
+
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        );
+    }
 }
